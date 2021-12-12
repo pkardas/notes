a779dec14cbcc267b5d1c56404a8724db197dce3
Piotr Kardaś <pkardas.it@gmail.com>
2021-12-12 10:55:13 +0100

    Added implementation of The Template Method Pattern

--- books/head-first-design-patterns.py
+++ books/head-first-design-patterns.py
@@ -690,3 +690,44 @@ class HomeTheaterFacade:
 
 home_theater = HomeTheaterFacade(*([Mock()] * 7))
 home_theater.watch_movie("Joker")
+
+
+# The Template Method Pattern:
+class CaffeineBeverage:
+    def prepare_recipe(self) -> None:
+        self._boil_water()
+        self._brew()
+        self._pour_in_cup()
+        self._add_condiments()
+
+    def _boil_water(self) -> None:
+        print("Boiling water")
+
+    def _pour_in_cup(self) -> None:
+        print("Pouring in a cup")
+
+    def _brew(self) -> None:
+        raise NotImplementedError
+
+    def _add_condiments(self) -> None:
+        raise NotImplementedError
+
+
+class Tea(CaffeineBeverage):
+    def _brew(self) -> None:
+        print("Steeping the tea")
+
+    def _add_condiments(self) -> None:
+        print("Adding Lemon")
+
+
+class Coffee(CaffeineBeverage):
+    def _brew(self) -> None:
+        print("Dripping Coffee through filter")
+
+    def _add_condiments(self) -> None:
+        print("Adding Sugar and Milk")
+
+
+Coffee().prepare_recipe()
+Tea().prepare_recipe()
